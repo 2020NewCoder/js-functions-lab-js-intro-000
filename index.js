2fa7b91194d9cbe6457ab6320d9cc8e96a8a8b63
KEITH ROGERS <asocialant@gmail.com>
2020-04-22 23:23:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,3 @@ function happyHolidays(name) {
   return ("Happy holidays!");
 }// write your code below!
 happyHolidays("name");
-
-function happyCustomHolidayTo(holiday, name){
-  return (holiday, name);
-}
-happyCustomHolidayTo(`Happy ${Independence Day}, you!`);
