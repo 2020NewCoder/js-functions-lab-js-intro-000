7f0b95ebc40ca93105079148e1f778297dbdc807
KEITH ROGERS <asocialant@gmail.com>
2020-04-23 00:45:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function happyHolidaysTo(name){
   return (`Happy holidays, ${name}!`)
 };
 happyHolidaysTo("you");
+
+function happyCustomHolidayto (holiday, name){
+  return (`Happy ${holiday}, ${name}!`)
+};
